02bd28dd6bc87e59c951378fd6773eb6c954e491
MdKasif0 <161304860+MdKasif0@users.noreply.github.com>
2024-11-22 22:01:26 +0530

    Update Black Forest Labs.js

--- Black Forest Labs.js
+++ Black Forest Labs.js
@@ -1,4 +1,4 @@
-const blackforestlabsApiKey = "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";
+const blackforestlabsApiKey = " Hugging_Face_API_KEY";
 const blackforestlabsModelUrl = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell";
 
 async function queryBlackForestLabs(data) {
